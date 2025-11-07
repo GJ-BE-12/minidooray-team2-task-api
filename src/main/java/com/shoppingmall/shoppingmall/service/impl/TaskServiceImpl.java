14dabbadd6b4336864413427b4b8c13aec85c1a7
TAEHUN <fbxogns321@naver.com>
2025-11-07 18:43:52 +0900

    pull전

--- src/main/java/com/shoppingmall/shoppingmall/service/impl/TaskServiceImpl.java
+++ src/main/java/com/shoppingmall/shoppingmall/service/impl/TaskServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
